55e87a2dddfbe4e17c364ce929f20ba0ad51a031
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2026-05-30 11:35:26 +0100

    added some jinja

---
+++ models/int_orders__pivoted.sql
@@ -0,0 +1,20 @@
+{%  set payment_methods=['credit_card', 'bank_transfer', 'coupon', 'gift_card'] %}
+
+with payments as 
+(
+    select * from {{ ref('stg_stripe__payments') }}
+    where status = 'success'
+),
+
+pivoted as (
+    select
+        order_id,
+        {% for method in payment_methods %}
+        sum(case when payment_method = '{{ method }}' then amount else 0 end) as {{ method }}_amount
+        {%- if not loop.last %},{% endif -%}
+        {% endfor %}
+    from payments
+    group by 1
+)
+
+select * from pivoted
--- models/scratch.sql
+++ models/scratch.sql
@@ -1,4 +1,5 @@
-{% set nob = 'smells' %}
+{#
+    {% set nob = 'smells' %}
 {% set alist = range(1,10) %}
 
 hello my nob is itchy
@@ -10,3 +11,26 @@ hello my nob is itchy
 {% else %}
 you probably have an STD
 {% endif %}
+
+##########################
+ 
+{% set animals = ['cat', 'dog', 'lemur', 'dodo' ] %}
+
+{%  for animal in animals %}
+my favorite animal is the {{ animal }}
+{% endfor %}
+#}
+
+with scratch as (
+    select distinct payment_method from {{ ref('stg_stripe__payments') }}
+)
+
+select * from scratch
+
+
+
+
+
+
+
+
